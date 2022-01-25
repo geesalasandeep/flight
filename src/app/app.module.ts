99760833b677f2826710fcc5caddfb6ab26f0e40
geesalasandeep <geesalasandeep@gmail.com>
2022-01-25 19:02:52 +0530

    flight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EuroairComponent } from './euroair/euroair.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EuroairComponent
   ],
   imports: [
     BrowserModule,
